5c91d16dcb49947137317171e65084cf2e4a8a80
Kumph <mphatsokumwendaec@gmail.com>
2023-07-26 12:12:24 -0700

    prompt

---
+++ print_prompt.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_prompt - prints the prompt to the terminal
+ */
+void print_prompt(void)
+{
+	char *prompt = "S_shell$ ";
+	int prompt_len = 9;
+
+	if ((isatty(STDIN_FILENO) == 1) && (isatty(STDOUT_FILENO) == 1))
+		flags.interactive = 1;
+
+	if (flags.interactive)
+	{
+		write(STDOUT_FILENO, prompt, prompt_len);
+	}
+	fflush(stdout);
+}
