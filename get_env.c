bf2b683be516371a4cb0182315e8b1fd6f8a4d75
Kumph <mphatsokumwendaec@gmail.com>
2023-07-26 12:11:19 -0700

    Get env and path

---
+++ get_env.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * _getenv - custom getenv
+ * @name: parameter
+ * Return: NULL
+ */
+char *_getenv(const char *name)
+{
+	size_t length = _strlen(name);
+	char **env = environ;
+
+	if (name == NULL)
+	{
+		return (NULL);
+	}
+
+	while (*env != NULL)
+	{
+		if (_strcmp(name, *env) == 0 && (*env)[length] == '=')
+		{
+			return (*env + length + 1);
+		}
+		env++;
+	}
+	return (NULL);
+}
---
+++ get_path.c
@@ -0,0 +1,51 @@
+#include "main.h"
+
+/**
+ * get_path - searches directories in PATH for cmd's executable file
+ * @cmd: command whose executable file to look for in PATH
+ * Return: path on sucess or NULL if not found
+ */
+char *get_path(char *cmd)
+{
+	char *path = _getenv("PATH"), *p, *p2 = NULL, *path_buffer;
+	struct stat st;
+
+	p2 = malloc(_strlen(path) + 1);
+	if (p2 == NULL)
+	{
+		perror("Error: unable to allocate memory");
+		return (NULL);
+	}
+	_strcpy(p2, path);
+	p = _strtok(p2, ":");
+	free(p2);
+	while (p != NULL)
+	{
+		path_buffer = malloc((_strlen(p) + _strlen(cmd) + 2) * sizeof(char));
+		if (path_buffer == NULL)
+		{
+			perror("Error: unable to allocate memory");
+			return (NULL);
+		}
+		_strcpy(path_buffer, p);
+		_strcat(path_buffer, "/");
+		_strcat(path_buffer, cmd);
+		if (stat(path_buffer, &st) == 0 && S_ISREG(st.st_mode))
+		{
+			char *result = malloc(_strlen(path_buffer) + 1);
+
+			if (result == NULL)
+			{
+				free(path_buffer);
+				return (NULL);
+			}
+			_strcpy(result, path_buffer);
+			free(path_buffer);
+			return (result);
+			free(result);
+		}
+		p = _strtok(NULL, ":");
+	}
+	perror("Error: Command not found");
+	return (NULL);
+}
